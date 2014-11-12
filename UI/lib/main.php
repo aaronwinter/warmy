b6e2c39881bf14849ccbf90c82bdba36640b6949
Aaron <eatounno@uwaterloo.ca>
2014-11-12 23:05:33 +0000

    implemented FetchJSON

--- UI/lib/main.php
+++ UI/lib/main.php
@@ -11,8 +11,35 @@
         echo "There is 13 new messages!";
     }
 
+
     function GenerateData($service, $output_context, $sizeOutput){
-        echo "<td>2119</td>";
-        echo "<td>European robot probe Philae has made the first, historic landing on a comet, after descending from its mothership</td>";
+
     }
+
+
+# Fetch JSON from $URL and return the data or NULL (if fetching failed) 
+# Example:
+# $a = FetchJSON("https://reddit.com/r/worldnews/top/.json");
+# $a is an associative array (i.e a dictionnary) containing the JSON fetched
+    function FetchJSON($URL, $output="associative"){
+        require 'vendor/autoload.php';
+        $headers  = array('Accept' => 'application/json');
+        $request  = Requests::get($URL, $headers, NULL);
+        if($request->status != "200"){
+            Log(__FUNCTION__, func_get_args(), $request->status);
+            echo "Failure";
+            return NULL;
+        }
+
+        $raw_data = $request->body;
+
+        if($output == "associative")
+            return $clean_data = json_decode($raw_data, $assoc=true);
+
+        else return $clean_data = json_decode($raw_data);
+    }
+
+function Log($function_name, $arguments, $extra){
+    return NULL;
+}
 ?>
