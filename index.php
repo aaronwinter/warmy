127f71fd705accac5add684d6a87dfac6dc01ea3
Aaron <eatounno@uwaterloo.ca>
2014-11-12 23:43:48 +0000

    Re-organization + test of small logical functions

--- index.php
+++ index.php
@@ -1,33 +1,124 @@
-<html>
-    <head>
-        <title>my smart homepage</title>
-    </head>
-    <body>
-        <header>
-            <h1>warmy</h1>
-            <hr />
-        </header>
+<?php
+    require 'lib/main.php';
+?>
 
-        <wrapper>
-            <facebook>
-            </facebook>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-            <reddit>
-            <?php
-               require 'vendor/autoload.php';
-               $headers  = array('Accept' => 'application/json');
-               $request  = Requests::get('https://reddit.com/r/worldnews/top/.json', $headers, NULL);
-               $raw_data = $request->body;
-                
-               $clean_data = json_decode($raw_data, $assoc=true);
-               $i = 0;
-               for($i = 0; $i < 25; $i++)
-                    echo '<p><a href="'.$clean_data["data"]["children"][$i]["data"]["domain"].'">'.$clean_data["data"]["children"][$i]["data"]["title"].'</a></p>';
-            ?>
-            </reddit>
+    <title>warmy - my smart homepage</title>
 
-            <mail>
-            </mail>
-        </wrapper>
-    </body>
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        body{
+            background-image: url('res/background.jpg');
+        }
+
+        source{
+            display: inline-block;
+            width: 100%;
+        }
+
+        info{
+            position: float;
+            width: 33%;
+            float: right;
+        }
+    </style>
+  </head>
+
+  <body style="background-image: url('res/background.jpg');">
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><?php echo date("M d Y") ?></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#"></a></li>
+            <li><a href="#about"></a></li>
+            <li><a href="#contact"></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container" style="background-color: rgba(0, 0, 0, 0.5); color: white;">
+
+      <div class="starter-template">
+        <h1>
+            <?php WelcomeMessage(); ?>,
+        </h1>
+
+        <p class="lead">
+            <?php (IsNewMessages()) ? (AlertNew()) : ("No new messages") ; ?>
+        <i class="icon-eye-open"></i></p> 
+        <source>
+          <info>
+            <table class="table table-hover">
+              <tbody>
+                <?php
+                    GenerateData("reddit", "table", 25);
+                ?>
+              </tbody>
+            </table>
+          </info>
+          <info>
+            <table class="table table-hover">
+              <tbody>
+                <?php
+                    GenerateData("reddit", "table", 25);
+                ?>
+              </tbody>
+            </table>
+          </info>
+          <info>
+            <table class="table table-hover">
+              <tbody>
+                <?php
+                    GenerateData("reddit", "table", 25);
+                ?>
+              </tbody>
+            </table>
+          </info>
+        </source>
+      </div>
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
--- lib/main.php
+++ lib/main.php
@@ -1,17 +1,65 @@
 <?php
+    function WelcomeMessage(){
+        echo "Good evening Aaron";
+    }
 
-function get_url($url){
-    $res = curl_init($url);
-    $timeout = 3;
+    function IsNewMessages(){
+        return false;
+    }
 
-    // Force curl to fetch fresh data (no-cache)
-    curl_setopt($res, CURLOPT_FRESH_CONNECT, true);
-    curl_setopt($res, CURLOPT_CONNECTTIMEOUT, $timeout); 
+    function AlertNew(){
+        echo "There is 13 new messages!";
+    }
 
-    $data = curl_exec($res);
-    curl_close($res);
 
-    return $data;
-}
+    function GenerateData($service, $output_context){
+        switch($service){
+            case "reddit":
+                $data = FetchJSON("https://reddit.com/r/worldnews/top/.json");
+                ($output_context == "table") ? PrettyTable($data) : $data;
+            break;
+        }
+    }
 
+
+# Fetch JSON from $URL and return the data or NULL (if fetching failed) 
+# Example:
+# $a = FetchJSON("https://reddit.com/r/worldnews/top/.json");
+# $a is an associative array (i.e a dictionnary) containing the JSON fetched
+    function FetchJSON($URL, $output="associative"){
+        require 'vendor/autoload.php';
+
+        $headers  = array('Accept' => 'application/json');
+        $request  = Requests::get($URL, $headers, NULL);
+
+        if($request->status_code != "200")
+            LogCrash(__FUNCTION__, func_get_args(), $request->status_code);
+
+        $raw_data = $request->body;
+
+        if($output == "associative")
+            return $clean_data = json_decode($raw_data, $assoc=true);
+
+        else return $raw_data; 
+    }
+
+# Output pretty rows that facilitate the integration to the UI
+    function PrettyTable($data){
+        $i = 0;
+        while ($i <= 24){
+            echo '<tr>';
+            echo '<td>'.$data["data"]["children"][$i]["data"]["ups"].'</td>';
+            echo '<td><a href="'.$data["data"]["children"][$i]["data"]["domain"].'" style="color: white;">"'.$data["data"]["children"][$i]["data"]["title"].'"</a></td>';
+            echo '</tr>';
+            $i++;
+        }
+    }
+
+# Log any abnormal interruption or misbehavor from functions to a log file
+# Example
+# if(Exception $e) LogCrash(__FUNCTION, func_get_args(), $e->getMessage());
+# LogCrash will write the function's name, its argument and the exception message
+    function LogCrash($function_name, $arguments, $extra = NULL){
+        return NULL;
+    }
 ?>
